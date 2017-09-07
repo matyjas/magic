0b99c932dc2cae55aad88aca6b87e24dfa31c486
Maciej Matyjas <matyjas@gmail.com>
2017-09-07 22:56:16 +0100

    purified test and added docs

--- env_for_tests.sh
+++ env_for_tests.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# run this file with source to place env var in scope
+
 export FACEBOOK_TOKEN="#local_fb_token_for_testing#"
--- test/fb/token_test.exs
+++ test/fb/token_test.exs
@@ -1,20 +1,21 @@
 defmodule FB.TokenVerifyTest do
   use MagicWeb.ConnCase, async: true
 
+  alias FB.{Token}
+  
   test "local token value exists" do
-    assert FB.Token.local_value != nil
-    assert is_binary FB.Token.local_value
+    assert Token.local_value != nil
+    assert is_binary Token.local_value
   end
   
   test "invalid token" do
-    challenged = "c-rock"
-    assert FB.Token.verify?("subscribe", "tokenized", challenged) ==
+    assert Token.verify?("subscribe", "tokenized", "not gonna happen") ==
     {:miss}
   end
 
   test "check fb values match" do
     challenged = "c-rock"
-    assert FB.Token.verify?("subscribe", FB.Token.local_value, challenged) ==
+    assert Token.verify?("subscribe", Token.local_value, challenged) ==
     {:match, challenged}
   end
   
