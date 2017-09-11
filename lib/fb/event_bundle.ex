cd6aaaf77afb6a6f15377674419955137a71eaae
Maciej Matyjas <matyjas@gmail.com>
2017-09-11 22:33:11 +0100

    did not pull out the right list of messages

--- lib/fb/event_bundle.ex
+++ lib/fb/event_bundle.ex
@@ -2,6 +2,8 @@ defmodule FB.EventBundle do
 
   def split(%{"entry" => entries, "object" => "page"}) do
     entries
+    |> Enum.map(&(&1["messaging"]))
+    |> Enum.concat
   end
   
   def split(_bundle) do
--- test/fb/event_bundle_test.exs
+++ test/fb/event_bundle_test.exs
@@ -9,21 +9,21 @@ defmodule FB.EventBundleTest do
   end
 
   test "sample update has one event" do
+    message = %{"message" =>
+		 %{"mid" => "mid.$cAABplXIuOB9ko-ghElecqLcyxWy_",
+		   "nlp" => %{"entities" => %{}}, "seq" => 7850,
+		   "text" => "what is the future?"},
+		"recipient" => %{"id" => "116090092394138"},
+		"sender" => %{"id" => "1646924535352518"},
+		"timestamp" => 1505161828626}
     entry =   %{"id" => "116090092394138",
-			     "messaging" =>
-			       [%{"message" =>
-				   %{"mid" => "mid.$cAABplXIuOB9ko-ghElecqLcyxWy_",
-				     "nlp" => %{"entities" => %{}}, "seq" => 7850,
-				     "text" => "what is the future?"},
-				  "recipient" => %{"id" => "116090092394138"},
-				  "sender" => %{"id" => "1646924535352518"},
-				  "timestamp" => 1505161828626}],
-		    "time" => 1505161829395}
+		"messaging" => [message],
+		"time" => 1505161829395}
     sample = %{"entry" => [entry],
 	       "object" => "page"}
-    entries = EventBundle.split(sample)
-    assert 1 == length entries
-    assert entry == hd entries
+    messages = EventBundle.split(sample)
+    assert 1 == length messages
+    assert message == hd messages
   end
 
 end
