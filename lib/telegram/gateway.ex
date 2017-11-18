fc8af13e965ac0cc4499bc0703007fb1e0bf6814
Maciej Matyjas <matyjas@gmail.com>
2017-11-18 21:35:57 +0000

    first step towards a GenServer-ed Gateway

--- lib/telegram/gateway.ex
+++ lib/telegram/gateway.ex
@@ -1,10 +1,28 @@
 defmodule Telegram.Gateway do
-
+  use GenServer
+  
   alias Telegram.{Token}
+
+  ## client side
+
+  def start_link(opts) do
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
   
   def send_message(sender_id, text) do
+    
+  end
+
+  ## server side callbacks
+
+  def init(:ok) do
+    {:ok, []}
+  end
+
+  def handle_cast({:send_message, to_id, message}, state) do
     url = "https://api.telegram.org/bot" <> Token.value <> "/sendMessage"
-    body = Poison.encode!(%{"chat_id": sender_id, "text": text})
+    body = Poison.encode!(%{"chat_id": to_id, "text": message})
     HTTPotion.post url, [body: body, headers: ["Content-Type": "application/json"]]
+    {:noreply, state}
   end
 end
