72728d5073640f0ae3001ccb746d4f57830643f3
Maciej Matyjas <matyjas@gmail.com>
2017-11-18 19:51:02 +0000

    wiring controller to message extraction

--- lib/magic_web/controllers/telegram_controller.ex
+++ lib/magic_web/controllers/telegram_controller.ex
@@ -1,9 +1,13 @@
 defmodule MagicWeb.TelegramController do
   use MagicWeb, :controller
 
+  alias Telegram.{Update}
+  
   def updates(conn, params) do
 
-#    IO.inspect params
+    params
+    |> Update.extract_sender_date
+    |> IO.inspect 
 
     text conn, "ok"
   end
