900b8c126aeac0340ba5eb6a18197b94d6f34fe1
Maciej Matyjas <matyjas@gmail.com>
2018-01-06 06:51:14 +0000

    required key added in setup

--- test/meditations/meditation_test.exs
+++ test/meditations/meditation_test.exs
@@ -3,8 +3,16 @@ defmodule Meditations.MeditationTest do
 
   alias Meditations.Meditation
 
-  test "has title" do
-    medi = %Meditation{}
-    assert nil != medi.title
+  setup do
+    body_scan = %Meditation{title: "Body Scan",
+			    date: "2018-01-06",
+			    duration: "20:47",
+			    description: "The body scan has proven to be an extremely powerful and healing form of meditation.",
+			    audio_url: "https://palousemindfulness.com/disks/bodyscan20min.mp3"}
+    {:ok, meditation: body_scan}
+  end
+  
+  test "has title", %{meditation: body_scan} do
+    assert nil != body_scan.title
   end
 end
