f094f1782c87178b3db7c8633cffad44491c65bf
Maciej Matyjas <matyjas@gmail.com>
2018-01-06 06:52:21 +0000

    just asking for feedback from users

---
+++ lib/feedback/prompt.ex
@@ -0,0 +1,16 @@
+defmodule Feedback.Prompt do
+
+  @moduledoc """
+  Entity for feedback text, used as a last resort if we dont know what a user wants
+  """
+  
+  @feedback_url "https://maciejmatyjas.typeform.com/to/HCTng6"
+  
+  @requests {"Please let us know how we are doing",
+	     "Feedback please",
+	     "Something appears to have gone wrong, your comments would be appreciated"}
+
+  def url(), do: @feedback_url 
+
+  def text(timestamp), do: elem(@requests, rem(timestamp, 3))
+end
