f1d9ad19dc5ee052659dd806c094497aeaa2c931
Maciej Matyjas <matyjas@gmail.com>
2018-01-27 06:56:34 +0000

    wiring telegram controller to dialog router started from supervisior

--- lib/magic_web/controllers/telegram_controller.ex
+++ lib/magic_web/controllers/telegram_controller.ex
@@ -1,12 +1,9 @@
 defmodule MagicWeb.TelegramController do
   use MagicWeb, :controller
-
-  alias Dialog.Router
   
   def updates(conn, params) do
 
-    {:ok, pid} = Router.start_link(message: Telegram.Update)
-    Router.route_request pid, params
+    Dialog.Router.route_request :telegram_router, params
     
     text conn, "ok"
   end
